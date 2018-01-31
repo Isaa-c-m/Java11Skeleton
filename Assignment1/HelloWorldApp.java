351e643b4619372e60cc677a368446931fdf2926
Isaa-c-m <isaac.mogerman@gmail.com>
2018-01-31 11:02:08 -0800

    compleated assighnment 1

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -1,2 +1,13 @@
-// Student name:
+// Student name:isaac mogerman
 // This is where your first assignment goes!
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("possem");
+        System.out.println("i dont know");
+        System.out.println("20");
+        System.out.println("i don know");
+        System.out.println("no");
+        System.out.println("2018-01-31");
+    }
+}
